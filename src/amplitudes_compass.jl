6a843ce274bd794584cf71553da1f9aa26a01597
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2018-03-13 16:53:16 +0100

    Speeded up PWA, plotted nicely

--- src/amplitudes_compass.jl
+++ src/amplitudes_compass.jl
@@ -1,10 +1,3 @@
-# module masses
-# global const mπ=0.139; global const mπ2=mπ^2;
-# global const mρ=0.7755; global const mρ2=mρ^2;
-# global const mτ=1.776; global const mτ2=mτ^2;
-# export mπ, mπ2, mρ, mρ2, mτ, mτ2
-# end
-#
 module amplitudes_compass
 # using masses: mπ, mπ2
 using DalitzPlotAnalysis: change_basis, Z, WignerDϵ, WignerD, ClebschGordon
@@ -130,7 +123,7 @@ isobarsS = [fσ,ff0_980,ff0_1500]
 
 basis = []
 wavenames = []
-let flat(σ1,cosθ1,ϕ1,cosθ23,ϕ23,m1sq,m2sq,m3sq,s) = 1
+let flat(σ1,cosθ1,ϕ1,cosθ23,ϕ23,m1sq,m2sq,m3sq,s) = 1.0+0.0im
     push!(basis,flat)
     push!(wavenames,"flat")
 end
@@ -140,7 +133,7 @@ for i in 2:size(wavesload,1)
     S = (S≥0 ? S : 0)
     @eval function $(Symbol("wave_$(wn)"))(σ1,cosθ1,ϕ1,cosθ23,ϕ23,m1sq,m2sq,m3sq,s)
 #         println("\nwave ",$J," ",$P," ",$M," ",$ϵ," ",$L," ",$S)
-        τ3 = [0.0,0.0,0.0,0.0]
+        τ3 = Vector{Float64}(4)
         σ3,τ3[1],τ3[2],τ3[3],τ3[4] = change_basis(σ1,cosθ1,ϕ1,cosθ23,ϕ23,m1sq,m2sq,m3sq,s)
         τ3[3] *= -1; τ3[4] += π
         R = 5;
@@ -153,7 +146,8 @@ for i in 2:size(wavesload,1)
     @eval push!(basis, $(Symbol("wave_$(wn)")))
 end
 
-function COMPASS_wave(i,s,σ1,cosθ1,ϕ1,cosθ23,ϕ23)
+function COMPASS_wave(i,s::Float64,σ1::Float64,
+    cosθ1::Float64,ϕ1::Float64,cosθ23::Float64,ϕ23::Float64)
     (i <  1) && return 0;
     (i > 88) && return 0;
     basis[i](σ1,cosθ1,ϕ1,cosθ23,ϕ23,mπ2,mπ2,mπ2,s)
@@ -162,116 +156,64 @@ end
 function COMPASS_waves(s,σ1,cosθ1,ϕ1,cosθ23,ϕ23)
     m1sq = mπ2; m2sq = mπ2; m3sq = mπ2;
     # system 1 <-> 23
-    Dϵ1 = [(M > J || λ > J) ? 0.0 : WignerDϵ(ϵ==1,J,M,λ,ϕ1,acos(cosθ1),0) for J=0:6, M=0:2, ϵ=0:1, λ=-3:3]
-    D1  = [(λ > S) ? 0.0 : WignerD(S,λ,0,ϕ23,acos(cosθ23),0) for S=0:6, λ=-3:3]
+    const Dϵ1 = [(M > J || λ > J) ? 0.0im : WignerDϵ(ϵ==1,J,M,λ,ϕ1,acos(cosθ1),0) for J=0:6, M=0:2, ϵ=0:1, λ=-3:3]
+    const D1  = [(λ > S) ? 0.0im : WignerD(S,λ,0,ϕ23,acos(cosθ23),0) for S=0:6, λ=-3:3]
     # Z functions
-    Zϵf1 = [0.5+0.0im];
+    const Zϵf1 = Vector{Complex{Float64}}(size(wavesload,1))
+    Zϵf1[1] = 0.5+0.0im;
+    const iV = [f(σ1) for f in isobarsV]
+    const iS = [f(σ1) for f in isobarsS]
+    const R = 5.0;
+    const bw1 = [(L == 0) ? 1.0 : BlttWskpf[L](λ(s,σ1,m1sq)/(4s)*R^2) for L=0:6];
     for i in 2:size(wavesload,1)
         wn, name, J, P, M, ϵ, S, L = wavesload[i,:]
-        fi = (S ≥ 0) ? isobarsV[S+1] : isobarsS[1-S]
+        fi = (S ≥ 0) ? iV[S+1] : iS[1-S]
         S = (S≥0 ? S : 0)
-        R = 5;
-        bw1 = (L == 0) ? 1.0 : BlttWskpf[L](λ(s,σ1,m1sq)/(4s)*R^2);
-        push!(Zϵf1, sum(ClebschGordon(L,0,S,lm,J,lm)*sqrt((2*L+1)*(2*S+1))*
+        Zϵf1[i] = sum(ClebschGordon(L,0,S,lm,J,lm)*sqrt((2*L+1)*(2*S+1))*
                         Dϵ1[J+1,M+1,1+(ϵ==P),4+lm]*D1[1+S,4+lm]
                          for lm=-min(S,J):min(S,J))*
-                fi(σ1)*bw1);
+                fi*bw1[L+1];
     end
     # system 3 <-> 12
-    τ3 = [0.0,0.0,0.0,0.0]
+    τ3 = Vector{Float64}(4)
     σ3,τ3[1],τ3[2],τ3[3],τ3[4] = change_basis(σ1,cosθ1,ϕ1,cosθ23,ϕ23,m1sq,m2sq,m3sq,s)
     τ3[3] *= -1; τ3[4] += π
     # functions
-    Dϵ3 = [(M > J || λ > J) ? 0.0 : WignerDϵ(ϵ==1,J,M,λ,τ3[2],acos(τ3[1]),0) for J=0:6, M=0:2, ϵ=0:1, λ=-3:3]
-    D3  = [(λ > S) ? 0.0 : WignerD(S,λ,0,τ3[4],acos(τ3[3]),0) for S=0:6, λ=-3:3]
+    const Dϵ3 = [(M > J || λ > J) ? 0.0im : WignerDϵ(ϵ==1,J,M,λ,τ3[2],acos(τ3[1]),0) for J=0:6, M=0:2, ϵ=0:1, λ=-3:3]
+    const D3  = [(λ > S) ? 0.0im : WignerD(S,λ,0,τ3[4],acos(τ3[3]),0) for S=0:6, λ=-3:3]
     # Z functions
-    Zϵf3 = [0.5+0.0im];
+    const Zϵf3 = Vector{Complex{Float64}}(size(wavesload,1))
+    Zϵf3[1] = 0.5+0.0im;
+    iV .= [f(σ3) for f in isobarsV]
+    iS .= [f(σ3) for f in isobarsS]
+    const bw3 = [(L == 0) ? 1.0 : BlttWskpf[L](λ(s,σ3,m3sq)/(4s)*R^2) for L=0:6];
     for i in 2:size(wavesload,1)
         wn, name, J, P, M, ϵ, S, L = wavesload[i,:]
-        fi = (S ≥ 0) ? isobarsV[S+1] : isobarsS[1-S]
+        fi = (S ≥ 0) ? iV[S+1] : iS[1-S]
         S = (S≥0 ? S : 0)
-        R = 5;
-        bw3 = (L == 0) ? 1.0 : BlttWskpf[L](λ(s,σ3,m3sq)/(4s)*R^2);
-        push!(Zϵf3, sum(ClebschGordon(L,0,S,lm,J,lm)*sqrt((2*L+1)*(2*S+1))*
+        Zϵf3[i] = sum(ClebschGordon(L,0,S,lm,J,lm)*sqrt((2*L+1)*(2*S+1))*
                             Dϵ3[J+1,M+1,1+(ϵ==P),4+lm]*D3[1+S,4+lm] for lm=-min(S,J):min(S,J))*
-                        fi(σ3)*bw3);
+                        fi*bw3[L+1];
     end
     (Zϵf1 + Zϵf3)
     # Zϵf1
 end
 
-# WignerDϵ(true,1,1,1,0.1,0.1,0.0)o
-# COMPASS_waves(1.4,0.6,0.1,0.1,0.1,0.1)
+# COMPASS_waves(1.4,0.6,0.1,0.1,0.1,0.1) - [COMPASS_wave(i,1.4,0.6,0.1,0.1,0.1,0.1) for i in 1:88]
+# # WignerDϵ(true,1,1,1,0.1,0.1,0.0)
+# @time for k = 1:1000
+#     COMPASS_waves(1.4,0.6,0.1,0.1,0.1,0.1)
+# end
+# # # # # #
+# @time for k = 1:1000
+#     [COMPASS_wave(i,1.4,0.6,0.1,0.1,0.1,0.1) for i in 1:88]
+# end
+# #
+# using Traceur
+# @trace COMPASS_waves(1.4,0.6,0.1,0.1,0.1,0.1)
+#
+# using BenchmarkTools
+# @btime [COMPASS_wave(i,1.4,0.6,0.1,0.1,0.1,0.1) for i in 1:88]
+# @btime COMPASS_waves(1.4,0.6,0.1,0.1,0.1,0.1)
 
 end
-
-
-# For three years I was deeply involved in the data reconstruction and analysis at VES experiment. My work was focused on the selection and analysis of the pi-pi0 system produced via peripheral scattering of the pion beam off a nucleon in the Beryllium target.
-# It was required select good event candidates from 10^7 data sample collected in 2011-2012. Neutral pion was reconstructed by the fit of energy clusters in the electromagnetic calorimeter. The shower profile was measured in with electron beam, calibration
-# The 1C-kinematical constraint for the pi0 mass was applied.
-
-
-
-# 1) Physics simulations
-#   I started my research with the physics simulation project which was suggested as a bachelor topic. The studies were dedicated to the idea of the active target detector for VES experiment. A bundle of the scintillating fibers was supposed to replace the target for the scattering experiment aiming to detect the recoil system.
-#   Later, I worked on Geant4-simulations of the lead converters to improve acceptance for the neutral particles at VES experiment.
-#   During my PhD, I had a chance to run Geant4 based COMPASS simulations. I implemented new event generator and advised a bachelor project for acceptance studies for the reaction pi- p -> pi- eta p at COMPASS.
-#
-# 2) Data reduction and numerical analysis
-#   During my master studies, I was deeply involved in the data reconstruction and analysis at VES experiment. My main project was the selection and analysis of the pi-pi0 system produced via peripheral scattering of the pion beam off a nucleon in the Beryllium target. It was required to select good event candidates from 10^7 data sample collected in 2011-2012.
-#
-#
-# 3)
-#
-# Neutral pion was reconstructed from showers information in electromagnetic calorimeter
-# The 1C-kinematical constraint for the pi0 mass was applied.
-#
-# The largest computation
-#
-# List of reactions I worked on, referenced below.
-# (1) pi-N->pi-pi0 N',
-# (2) pi-p->pi-eta p', eta->gg, eta->3pi,
-# (3a/b) pi-p->3pi/pi-2pi0 p',
-# (4a/b) tau -> 3p/pi-2pi0i nu,
-# (5) B->J/Psi pi K
-# (6) e+e-->J/Psi pi+pi-
-# 
-# 1) Physics simulations
-#   = my bachelor project was dedicated to the studies of the active target detector. I worked on the Geant4 model and reconstruction algorithm for the bundle of the scintillating fibers used as the recoil detection system.
-#   = I worked on Geant4-simulations of the lead converters to improve acceptance for the neutral particles at VES experiment.
-#   = Acceptance studies for (1) was a part of my master project.
-#   = Geant4 based COMPASS simulations. I implemented new event generator and advised a bachelor project for acceptance studies for the reaction (2) at COMPASS.
-#
-# 2) Data reduction and numerical analysis
-#   = I was involved in the reconstruction and analyses at VES experiment. I performed the event selection and the partial wave analysis of the pi-pi0 in reaction (1). The significant background from the reaction (3) was simulated and subtracted.
-#   = My work at COMPASS was focused on the numerical analyses related to the reaction (3a). I tested various theoretical models to the dynamics in partial waves on the COMPASS data. Pole positions for a1 and pi2 mesons were obtained. The exotic meson candidate was described as the rescattering phenomenon.
-#
-# 3) Theoretical calculations:
-#   = Rescattering corrections in the system of three pions for the reaction (3a).
-# Effective Lagrangian and dispersive theory.
-#   = Tensor and helicity formalisms. The amplitude model for the reaction (5).
-#
-# 4) Teaching and advising
-#   = Lecturer at the Reaction Theory School
-#   = Advising 3 master- and 1 bachelor- students
-#
-#
-#   "Nature of the a1(1420)", Phys. Rev. D91.9, p. 094015, 3 authors, inspire id 1341619.
-#   personal contribution: all calculations, text
-#
-#   "What is the right formalism to search for resonances?" arXiv: 1712.02815 [hep-ph], accepted by EPJC, 10 authors, inspire id 1642229
-#   personal contribution: all calculations, text
-#
-#   "New analysis of etapi tensor resonances measured at the COMPASS
-#   experiment", arXiv: 1707.02848 [hep-ph], 235 authors, inspire id 1609260
-#   personal contribution: cross-check of calculation and fit results, corrections to the text
-#
-#   "Amplitude analysis and the nature of the Zc(3900)", Phys. Lett.
-#   B772, pp. 200–209, 7 authors, inspire id 1505197
-#   personal contribution: cross-check of calculation, corrections to the text
-#
-#   "Light isovector resonances in pi-pp>3pi p at 190 GeV/c", arXiv:1802.05913 [hep-ex], sent to EPJC, 214 authors, inspire id 1655631
-#   personal contribution: appendix A, corrections to text
-#
-#   "Unitarity approach to the mass-dependent fit of 3pi resonance production data from the COMPASS experiment", EPJ Web Conf. 137, p. 05017, 4 authors, inspire id 1519590
-#   personal contributions: formalism, calculations, fits, text.
