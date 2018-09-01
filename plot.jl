47c66461e496ca2f38453f1ba6c602dfc5886d89
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2018-09-01 18:59:38 +0200

    Shaped code a bit

--- plot.jl
+++ plot.jl
@@ -1,12 +1,12 @@
 using Plots
 
-function CreatePlot(i::Int64, fileextra = "", filefolder = "output", output::Bool = true)
-  """
+"""
   i: index of the wave,
   fileextra: additional text in the output-filename (default "")
   folder: name of the folder where the data files are located (default "output")
   output: boolean to decide if the plot should be saved in the outputfile (default true)
-  """
+"""
+function CreatePlot(i::Int64, fileextra = "", filefolder = "output", output::Bool = true)
   stephan = readdlm("/mnt/data/compass/2008/integrals_stefan/h$(i).n.out")
   misha = readdlm("$(filefolder)/h$(i).txt")
   stephan[2:202,2] ./= (stephan[2:202,1])
@@ -17,7 +17,7 @@ function CreatePlot(i::Int64, fileextra = "", filefolder = "output", output::Boo
   let p = sum(misha[:,2])
     plot!(m3π, misha[:,2]./p,label="misha")
   end
-  if output 
+  if output
     savefig("misha_vs_stephan$(fileextra)$(i).pdf")
   end
   plot!()
@@ -27,23 +27,24 @@ for i in [2]
   CreatePlot(i, "", "output_new", false)
 end
 
+readdlm("output_new/h2.txt")
 function CreatePlotInterference(i_misha::Int64, j_misha::Int64, i_florian::Int64, j_florian::Int64, fileextra = "", filefolder = "output", output::Bool = true)
   mishai = readdlm("$(filefolder)/h$(i_misha).txt")
   mishaj = readdlm("$(filefolder)/h$(j_misha).txt")
   mishaij = readdlm("$(filefolder)/h$(i_misha)_$(j_misha).txt")
-  
+
   mishaij[:,2:3] ./= sqrt.(mishai[:,2].*mishaj[:,2])
-  plot(mishaij[1:200,1], -mishaij[1:200,2], label="-re(misha)")
-  plot!(mishaij[1:200,1], mishaij[1:200,3], label="im(misha)")
-  
+  plot( mishaij[:,1], mishaij[:,2], label="re(misha)", frame=:origin)
+  plot!(mishaij[:,1], mishaij[:,3], label="im(misha)")
+
   florian = readdlm("diag.txt")
   florianij = readdlm("offdiag_$(i_florian)_$(j_florian).txt")
   florianij[:,2:3] = florianij[:,2:3]./sqrt.(florian[i_florian+1,2:201].*florian[j_florian+1,2:201])
-  plot!(florianij[:,1], florianij[:,2], label="re(florian)", title="Interference:  $(florian[i_florian+1,1])...$(florian[i_florian+1,1])")
+  plot!(florianij[:,1], florianij[:,2], label="re(florian)", title="Interference:  $(florian[i_florian+1,1])...$(florian[j_florian+1,1])")
   plot!(florianij[:,1], florianij[:,3], label="im(florian)")
-  if output
-    savefig("interference_$(fileextra)$(i_misha)_$(j_misha).pdf")
-  end
+  # if output
+  #   savefig("interference_$(fileextra)$(i_misha)_$(j_misha).pdf")
+  # end
   plot!()
 end
 
--- test.jl
+++ test.jl
@@ -1,11 +1,12 @@
 ## definitions
+push!(LOAD_PATH,"src")
 using DalitzPlotAnalysis
 using amplitudes_compass
 
 using Cuba
 using Plots
 
-m3π = collect(0.5:0.01:2.5)
+m3π = collect(0.5:0.5:2.5)
 
 Φ(s, m1sq, m2sq) = (s > (sqrt(m1sq)+sqrt(m2sq))^2) ? sqrt(λ(s, m1sq, m2sq))/(8 * π * s) : 0
 function everything(i::Int64, folder = "output")
@@ -13,7 +14,7 @@ function everything(i::Int64, folder = "output")
     function integrand(x,f)
       s3min = 4.0 * mπ2
       s3max = (sqrt(s) - mπ)^2
-      
+
       s3 =  s3min + x[1] * (s3max - s3min)
       cosθ1 = -1.0 + 2.0*x[2]
       ϕ1 = -π + 2.0*π*x[3]
@@ -22,14 +23,13 @@ function everything(i::Int64, folder = "output")
       a = COMPASS_wave(i, s, s3, cosθ1, ϕ1, cosθ23, ϕ23)
       f[1] = abs2(a) * Φ(s, s3, mπ2) * Φ(s3, mπ2, mπ2)*(s3max-s3min)
     end
-    return cuhre(integrand, 5, 1)
+    return cuhre(integrand, 5, 1)[1][1]
   end
 
-  preresult = total.(m3π.^2, i)
-  result = []
-  for index in 1:length(preresult)
-        push!(result,preresult[index][1][1])
-      end
+  result = Array{Float64}(length(m3π));
+  @progress for (p,msq) in enumerate(m3π.^2)
+    result[p] = total(msq, i)
+  end
 
   writedlm("$(folder)/h$(i).txt",[m3π result])
   return result
@@ -37,19 +37,25 @@ end
 ## calculating integrals
 
 
-for index in [2]
+for index in [2,11]
   everything(index, "output_new")
 end
 
+# let s = 0.257672, τ1 = [0.100016 -0.997405 -0.694468 0.0442564 -1.24848],
+#   τ3 = [0.109153 0.529428 -2.63108 0.14344 -π+0.0805736]
+#   τ3p = change_basis(τ1...,mπ2,mπ2,mπ2,s)
+#   # τ2 = change_basis(τ3...,mπ2,mπ2,mπ2,s)
+#   # τ1p = change_basis(τ2...,mπ2,mπ2,mπ2,s)
+#   [τ3[i]-τ3p[i] for i in 1:5]
+# end
 
-
-## interference 
+## interference
 function interference(i::Int64, j::Int64, folder = "output")
   function total2(s, i::Int64, j::Int64)
     function integrand2(x,f)
       s3min = 4.0 * mπ2
       s3max = (sqrt(s) - mπ)^2
-      
+
       s3 =  s3min + x[1] * (s3max - s3min)
       cosθ1 = -1.0 + 2.0*x[2]
       ϕ1 = -π + 2.0*π*x[3]
@@ -59,16 +65,15 @@ function interference(i::Int64, j::Int64, folder = "output")
       b = COMPASS_wave(j, s, s3, cosθ1, ϕ1, cosθ23, ϕ23)
       f[1], f[2] = reim(a * conj(b) * Φ(s, s3, mπ2) * Φ(s3, mπ2, mπ2)*(s3max-s3min))
     end
-    return cuhre(integrand2, 5, 2)
+    return complex(cuhre(integrand2, 5, 2)[1]...)
   end
 
-  preresult = total2.(m3π.^2, i, j)
-  resultre = []
-  resultim = []
-  for index in 1:length(preresult)
-    push!(resultre,preresult[index][1][1])
-    push!(resultim,preresult[index][1][2])
+  preresult = Array{Complex{Float64}}(length(m3π));
+  @progress for (p,msq) in enumerate(m3π.^2)
+    preresult[p] = total2(msq, i, j)
   end
+  resultre = real.(preresult)
+  resultim = imag.(preresult)
 
   writedlm("$(folder)/h$(i)_$(j).txt",[m3π resultre resultim])
   return resultre, resultim
