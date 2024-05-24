2375687167ac01db6f64ae4a1fb669f5ab56f753
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-05-25 01:19:05 +0200

    recover some SDMCalc functionality

--- examples/SDMCalculator.jl
+++ examples/SDMCalculator.jl
@@ -1,22 +1,21 @@
 # parameters
-mass_bin_name = ARGS[1] # "1540_1560"
+mass_bin_name = "1540_1560"
 tslice = "t1"
 path_wavelist = "src"
 path_to_working_folder = "data"
+
 const M3pi = Meta.parse(mass_bin_name[1:4]) / 1000
 const Nwave_extended = 88
 #########################################################
 
+using PartialWavesFromScratch.SDMHelper
+using PartialWavesFromScratch.PWAHelper
+using PartialWavesFromScratch.amplitudes_compass
+using PartialWavesFromScratch.PlotHelper
 
-@show ARGS
-push!(LOAD_PATH, "src")
-using SDMHelper
-using PWAHelper
-using amplitudes_compass
 using DelimitedFiles
 using Plots
 using LinearAlgebra
-using PlotHelper
 
 # MAIN FIT
 ##### build the model:
@@ -24,13 +23,16 @@ using PlotHelper
 # set names
 for app in ["rd", "mc", "fu"]
     pwf = path_to_working_folder
-    @eval $(Symbol("basisfunc_" * app)) = joinpath($pwf, "variables_$(mass_bin_name)_$(tslice)_" * $app * ".txt")
+    @eval $(Symbol("basisfunc_" * app)) = joinpath($pwf, "variables_$(mass_bin_name)_$(tslice)_" * $app * ".bin")
 end
+
 # calculate normfact by getting number of lines
-nlines_rd = Meta.parse(split(read(`wc -l $(basisfunc_rd)`, String), " ")[1])
-nlines_mc = Meta.parse(split(read(`wc -l $(basisfunc_mc)`, String), " ")[1])
-nlines_fu = Meta.parse(split(read(`wc -l $(basisfunc_fu)`, String), " ")[1])
+nlines_rd = read(open(basisfunc_rd, "r"), Int32)
+nlines_mc = read(open(basisfunc_mc, "r"), Int32)
+nlines_fu = read(open(basisfunc_fu, "r"), Int32)
+
 normfact = nlines_fu / nlines_mc * nlines_rd
+
 # read matrix of integrals
 BmatFU = read_cmatrix("data/integrmat_$(mass_bin_name)_$(tslice)_fu.txt");
 size(readdlm(joinpath(path_wavelist, "wavelist_formated.txt")), 1)
@@ -38,7 +40,7 @@ size(readdlm(joinpath(path_wavelist, "wavelist_formated.txt")), 1)
 wavelist = get_wavelist(joinpath(path_wavelist, "wavelist_formated.txt");
     path_to_thresholds=joinpath(path_wavelist, "thresholds_formated.txt"),
     M3pi=M3pi)
-const noϵ = [1] # flat wave
+# 
 const posϵ = [i for (i, ϵ) in enumerate(wavelist[:, 6]) if ϵ == "+"]
 const negϵ = [i for (i, ϵ) in enumerate(wavelist[:, 6]) if ϵ == "-"]
 const ModelBlocks = [noϵ, posϵ, negϵ, negϵ]
