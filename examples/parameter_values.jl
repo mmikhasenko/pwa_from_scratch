90836b7a6d3d00a4c37a8791bf96a843bfb27a98
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-05-26 11:59:35 +0200

    complete validation output

--- examples/parameter_values.jl
+++ examples/parameter_values.jl
@@ -17,6 +17,7 @@ using PartialWavesFromScratch.PlotHelper
 using DelimitedFiles
 using Plots
 using LinearAlgebra
+using JSON
 
 # ## parameters
 # To get the parameters from the matrix, one needs:
@@ -24,28 +25,13 @@ using LinearAlgebra
 # - matrix of integrals
 # - model description as a ModelBlocks
 
-
-
-# set names
-normfact = let
-    for app in ["rd", "mc", "fu"]
-        pwf = path_to_working_folder
-        @eval $(Symbol("basisfunc_" * app)) = joinpath($pwf, "variables_$(mass_bin_name)_$(tslice)_" * $app * ".bin")
-    end
-    # calculate normfact by getting number of lines
-    nlines_rd = read(open(basisfunc_rd, "r"), Int32)
-    nlines_mc = read(open(basisfunc_mc, "r"), Int32)
-    nlines_fu = read(open(basisfunc_fu, "r"), Int32)
-    nlines_fu / nlines_mc * nlines_rd
-end
-
-
 # Model description
 wavelist = get_wavelist(joinpath(path_wavelist, "wavelist_formated.txt");
     path_to_thresholds=joinpath(path_wavelist, "thresholds_formated.txt"),
     M3pi)
 # 
 const ModelBlocks = let
+    noϵ = [1] # flat wave
     posϵ = [i for (i, ϵ) in enumerate(wavelist[:, 6]) if ϵ == "+"]
     negϵ = [i for (i, ϵ) in enumerate(wavelist[:, 6]) if ϵ == "-"]
     [noϵ, posϵ, negϵ, negϵ]
@@ -63,8 +49,21 @@ end
 
 # read matrix of integrals
 BmatFU = read_cmatrix("data/integrmat_$(mass_bin_name)_$(tslice)_fu.txt");
-pars0 = SDM_to_pars(SDM_RD ./ normfact, BmatFU, ModelBlocks)
-pars_to_SDM
+# set names
+normfact = let
+    nlines_rd, nlines_mc, nlines_fu =
+        map(["rd", "mc", "fu"]) do ext
+            filename =
+                joinpath(path_to_working_folder,
+                    "variables_$(mass_bin_name)_$(tslice)_") *
+                ext * ".bin"
+            # calculate normfact by getting number of lines
+            nlines = read(open(filename, "r"), Int32)
+        end
+    nlines_fu / nlines_mc * nlines_rd
+end
+BmatFU_norm = BmatFU .* normfact
+pars0 = SDM_to_pars(SDM_RD, BmatFU_norm, ModelBlocks)
 
 weights = let
     pars = pars0
@@ -75,20 +74,32 @@ weights = let
     bl = pblocks[2]
     pars_bl[bl] .= pars[bl]
     shrnk(pars_bl, Tmap)
-    # 
 end
 
-
 basis = get_wavebasis(wavelist)
-weights
 
-
+# reference point
 
 τ1_test = (
     σ1=0.6311001857724697,
     cosθ1=-0.36619233111451877, ϕ1=0.09298675596700612,
     cosθ23=-0.611301179735489, ϕ23=0.6244178754076133, s=2.3253174651821458)
 # 
-sum(zip(weights, basis)) do (w, b)
-    w * b(τ1...)
+references = map(b -> b(τ1_test...), basis)
+
+# 
+waves_summary = DataFrame(wavelist, [:wn, :name, :J, :P, :M, :ϵ, :S, :L])
+waves_summary.weights = weights
+waves_summary.references = references
+
+let
+    folder = joinpath(@__DIR__, "..", "tests", "references")
+    filename = joinpath(folder, mass_bin_name * "_ref.json")
+    _waves_summary = transform(waves_summary,
+        :references => ByRow(x -> string(x)) => :references,
+        :weights => ByRow(x -> string(x)) => :weights
+    )
+    open(filename, "w") do io
+        JSON.print(io, _waves_summary, 4)
+    end
 end
